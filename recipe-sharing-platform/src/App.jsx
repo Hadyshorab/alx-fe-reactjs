46434dfad9580301b493f2b59625a6aedec0cc8d
Hadyshorab <hadyshorab4@gmail.com>
2024-09-26 21:00:14 +0300

    App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,22 +1,30 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
-import Counter from './components/Counter'
-
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
-    <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
-      <Header />
-      <MainContent />
-      <WelcomeMessage />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
-      <Counter />
-      <Footer />
-    </div>
-  )
+    <Router>
+      <ul className='flex justify-center gap-2'>
+        <Link
+          to={"/"}
+          className='text-white bg-indigo-700 mt-4 inline-block p-2 rounded-lg hover:bg-indigo-950'>
+          Home
+        </Link>
+        <Link
+          to={"/RecipeForm"}
+          className='text-white bg-indigo-700 mt-4 inline-block p-2 rounded-lg hover:bg-indigo-950'>
+          Add recipe
+        </Link>
+      </ul>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/recipe/:id' element={<RecipeDetail />} />
+        <Route path='/RecipeForm' element={<AddRecipeForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
