fa37534553a68a670facd684b2be787c2a0ff2bb
Hadyshorab <hadyshorab4@gmail.com>
2024-09-26 18:22:59 +0300

    App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,22 +1,34 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
-import Counter from './components/Counter'
-
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Profile from "./components/Profile";
+import Blog from "./components/blog"
+import BlogPost from "./components/BlogPost.JSX";
+import { AuthContext, useAuth } from "./components/ProtectedRoute";
+import Login from "./components/Login";
 
 function App() {
+  const auth = useAuth();
+
   return (
-    <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
-      <Header />
-      <MainContent />
-      <WelcomeMessage />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
-      <Counter />
-      <Footer />
-    </div>
-  )
+    <Router>
+      <AuthContext.Provider>
+      <div>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/Profile'>Profile</Link>
+          <Link to='/login'>Login</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<h2>Home</h2>} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
+          <Route path='/profile/*' element={<Profile />} />
+          {!auth.user && (<Route path='/login' element={<Login />} />)}
+          
+        </Routes>
+      </div>
+      </AuthContext.Provider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
