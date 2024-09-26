9bdc07d1564f79332d3cd36ec1b3194dabd9026c
Hadyshorab <hadyshorab4@gmail.com>
2024-09-26 18:57:29 +0300

    UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,11 +1,13 @@
-const UserProfile = (prop) => {
+function UserProfile() {
     return (
-      <div style={{border: "1px solid gray", padding: "10px", margin: "10px"}}>
-        <h2 style={{color: "blue"}}>{prop.name}</h2>
-        <p>Age: <span style={{ fontWeight: 'bold' }}>{prop.age}</span></p>
-        <p>Bio: {prop.bio}</p>
+      <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 sm:max-w-xs md:p-8 md:max-w-sm hover:shadow-xl">
+        <img src="https://placehold.co/150" alt="User"  className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out" />
+        <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500 cursor-pointer">John Doe</h1>
+        <p className="text-gray-600 text-base sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
-  };
-
-  export default UserProfile
+  }
+  
+  
+  export default UserProfile;
+  
